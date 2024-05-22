997518b1e376bb07617a5c29f09a916f3aae1d49
JaiZemoso <jai.jayanthilal@zemosolabs.com>
2024-05-22 12:46:52 +0530

    first commit

---
+++ src/com/practice/arrays/ReverseSubArrayInGroups.java
@@ -0,0 +1,16 @@
+package com.practice.arrays;
+
+import java.util.ArrayList;
+
+public class ReverseSubArrayInGroups {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	void reverseInGroups(ArrayList<Integer> arr, int n, int k) {
+        // code here
+    }
+
+}
